c16b2b037a6d7e98bbda950feb6bb8473e87245e
nnian <48366474+nnian@users.noreply.github.com>
2020-08-08 13:40:17 +0200

    Update config.js

--- config.js
+++ config.js
@@ -24,9 +24,8 @@ var nodesStat =
 	[
 		["node-00.qwertycoin.org:8197"],
 		["node-01.qwertycoin.org:8197"],
-		["https://node-02.qwertycoin.org"],
+		["node-02.qwertycoin.org:8197"],
 		["node-03.qwertycoin.org:8197"],
 		["node-04.qwertycoin.org:8197"],
-		["node-05.qwertycoin.org:8197"],
-		["https://api.qwertycoin.org/sslnode"]
+		["node-05.qwertycoin.org:8197"]
     ];

7799fccc85b253e0576450cd02e2762acd01bde2
nnian <48366474+nnian@users.noreply.github.com>
2020-10-27 23:52:43 +0100

    update pool lists

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var api = "https://api.qwertycoin.org/sslnode";
 var apiList = ["https://api.qwertycoin.org/sslnode"];
 var blockTargetInterval = 120;
-var coinUnits = 100000000;
+var coinUnits = 10000;
 var symbol = 'QWC';
 var refreshDelay = 30000;
 var addressPattern = new RegExp("^QWC[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{95}$");
@@ -10,13 +10,13 @@ var poolsStat =
 	[
 		["pool.qwertycoin.org","https://pool.qwertycoin.org:8119/stats"],
 		["superblockchain.con-ip.com/qwc","https://superblockchain.con-ip.com:8333/stats"],
-		["qwc.superpools.online","https://qwc.cryptonote.club:8199/stats"],
+		//["qwc.superpools.online","https://qwc.cryptonote.club:8199/stats"],
 		["easyhash.pro/qwc","https://easyhash.pro/qwc/api/stats"],
-		["qwc.cryptonote.club","https://qwc.cryptonote.club:8199/stats"],
+		//["qwc.cryptonote.club","https://qwc.cryptonote.club:8199/stats"],
 		["qwertycoin.fairhash.org","https://qwertycoin.fairhash.org/api/stats"],
 		["newpool.pw/qwc_b2b","https://minenice.newpool.pw:8205/stats"],
 		["dxpool.com","https://api.qwertycoin.org/pools/dxpool-com"],
-		["m2pool.eu","http://103.253.41.40:9981/stats"],
+		["m2pool.eu","http://ro.qwc.m2pool.eu:9981"],
 		//["qwertycoin.herominers.com","https://qwertycoin.herominers.com/api/stats"]
 		//["qwertypool.com","https://qwertypool.com:7776/stats"]
     ];
